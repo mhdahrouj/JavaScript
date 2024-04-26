84ad4f99702e1a10f50c25675a4bb2d8c1201be7
Hassn Dahrouj <165599571+mhdahrouj@users.noreply.github.com>
2024-04-26 15:14:34 +0200

    solve the exercises

--- Week1/prep-exercises/1-traffic-light/traffic-light-1.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-1.js
@@ -16,6 +16,20 @@ while (rotations < 2) {
   // if the color is green, turn it orange
   // if the color is orange, turn it red
   // if the color is red, add 1 to rotations and turn it green
+
+  let rotations = 0;
+  while (rotations < 2) {
+    let currentState = trafficLight.state;
+    console.log("The traffic light is on", currentState);
+    if (currentState === "green") {
+      trafficLight.state = "orange";
+    } else if (currentState === "orange") {
+      trafficLight.state = "red";
+    } else if ((currentState = "red")) {
+      trafficLight.state = "green";
+      rotations++;
+    }
+  }
 }
 
 /**
--- Week1/prep-exercises/1-traffic-light/traffic-light-2.js
+++ Week1/prep-exercises/1-traffic-light/traffic-light-2.js
@@ -18,6 +18,20 @@ while (cycle < 2) {
   // if the color is green, turn it orange
   // if the color is orange, turn it red
   // if the color is red, add 1 to cycles and turn it green
+
+  let cycle = 0;
+  while (cycle < 2) {
+    const currentState = trafficLight.possibleStates[trafficLight.stateIndex];
+    console.log("the traffic light is on", currentState);
+    if (currentState === "green") {
+      trafficLight.stateIndex = 1;
+    } else if (currentState === "orange") {
+      trafficLight.stateIndex = 2;
+    } else if (currentState === "red") {
+      trafficLight.stateIndex = 0;
+      cycle++;
+    }
+  }
 }
 
 /**
